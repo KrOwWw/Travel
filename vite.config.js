61976b6066c645c9bac36c3735105365a2580051
Ruslan <qwertyuiopakustalb@mail.ru>
2024-11-19 18:46:01 +0300

    Fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	root: './src',
-	base: '/Travel/',
+	base: '/travel/',
 	publicDir: './src/assets/',
 	server: {
 		port: 3000,
